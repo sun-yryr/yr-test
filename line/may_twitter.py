b1d6582b0184aea09140273e440fe6813461164f
yryr <taittide@gmail.com>
2017-10-28 02:03:18 +0900

    twitter

--- line/may_twitter.py
+++ line/may_twitter.py
@@ -4,23 +4,20 @@ import requests
 import sys
 import json
 from requests_oauthlib import OAuth1Session
-from time import sleep
 import config
 
 def main(oauth):
     url_tweet = "https://api.twitter.com/1.1/statuses/update.json"
     url_read = "https://api.twitter.com/1.1/statuses/home_timeline.json"
     url_myread = "https://api.twitter.com/1.1/statuses/user_timeline.json"
-    #tweet = raw_input("tweet>> ")
-    #params = {"status": tweet}
     
     f = open("id.txt","r+")
     Twi_id = f.read()
     
     params = {
             "user_id":3404290100,
-            "count":200,
-            #"since_id":Twi_id,
+            "count":20,
+            "since_id":Twi_id,
             #"include_rts":False
             }
     req = oauth.get(url_myread, params = params)
@@ -31,19 +28,11 @@ def main(oauth):
         # 各ツイートの本文を表示
         for tweet in reversed(timeline):
             user = tweet["user"]
-        #   if tweet["text"].find("RT") == 0:
-        #       print(tweet["text"].split(":",1)[1])
-        #       print("------------------------------")
-        #   if tweet["text"].find(unicode("#ゆるゆるめも",'utf-8')) != -1:
-        #       print("ok")
-        #       print("------------------------------")
             toukou = "[" + user["name"] + "]\n" + tweet["text"]
-            print(toukou)
-            print("------------------------------")
-            #notify(toukou)
-            #sleep(5)
-        f.seek(0)
-        f.write(str(tweet["id"]))
+            notify(toukou)
+        if len(timeline) != 0:
+            f.seek(0)
+            f.write(str(tweet["id"]))
     else:
         print ("Error: %d" % req.status_code)
     f.close
@@ -51,7 +40,7 @@ def main(oauth):
 
 def notify(text):
     url = "https://notify-api.line.me/api/notify"
-    token = LINE_token
+    token = config.LINE_token
     headers = {"Authorization" : "Bearer" + " " + token}
     payload = {"message" :  text}
     
