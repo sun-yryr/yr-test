7baaf9f8f30ac54aa2001a32c229ddf47530e211
yryr <taittide@gmail.com>
2017-10-29 06:35:41 +0900

    image add

--- line/may_twitter.py
+++ line/may_twitter.py
@@ -1,5 +1,5 @@
 # coding:utf-8
-#自分のツイートの中からRTと指定タグを検出
+#対象のツイートを通知、画像対応
 import requests
 import sys
 import json
@@ -15,11 +15,12 @@ def main(oauth):
     Twi_id = f.read()
     
     params = {
-            "user_id":3404290100,
-            "count":20,
-            "since_id":Twi_id,
-            #"include_rts":False
-            }
+            #"user_id":3404290100,
+            "count":1,
+            #"since_id":Twi_id,
+            "exclude_replies":True,
+            "include_rts":False
+        }
     req = oauth.get(url_myread, params = params)
 
     # レスポンスを確認
@@ -29,35 +30,47 @@ def main(oauth):
         for tweet in reversed(timeline):
             user = tweet["user"]
             toukou = "[" + user["name"] + "]\n" + tweet["text"]
-            notify(toukou)
+            notify(toukou,None)
+
+            try:
+                entities = tweet["extended_entities"]
+                for media in entities["media"]:
+                    media_url = media["media_url"]
+                    notify(media_url,media_url)
+            except:
+                madia_url = None
         if len(timeline) != 0:
             f.seek(0)
-            f.write(str(tweet["id"]))
+            f.write(tweet["id_str"])
     else:
         print ("Error: %d" % req.status_code)
     f.close
 
 
-def notify(text):
+def notify(text,media_url):
     url = "https://notify-api.line.me/api/notify"
     token = config.LINE_token
     headers = {"Authorization" : "Bearer" + " " + token}
-    payload = {"message" :  text}
-    
-    r = requests.post(url ,headers = headers ,params=payload)
+    payload = {
+                "message" : text,
+                "imageThumbnail" : media_url,
+                "imageFullsize" : media_url
+                }
+    r = requests.post(url ,headers = headers ,params = payload)
+    print r.status_code
 
 
 if __name__ == '__main__':
     try:
         oauth = OAuth1Session(
-                   config.Twi_CK,
-                   config.Twi_CS,
-                   config.Twi_AT,
-                   config.Twi_ATS
-                   )
+                              config.Twi_CK,
+                              config.Twi_CS,
+                              config.Twi_AT,
+                              config.Twi_ATS
+                              )
     except:
         print "oauth error\n"
         sys.exit(1)
-
+    
     main(oauth)
 
